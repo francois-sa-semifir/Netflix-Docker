b150d2625841b58f76352f66e5dbc1ac3ee6a849
Francois Salmon <francois.salmon@semifir.com>
2026-09-10 15:01:22 +0200

    refactor: control flow migration *ngIf/*ngFor → @if/@for

--- src/app/list-film/list-film.component.ts
+++ src/app/list-film/list-film.component.ts
@@ -1,13 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-inferrable-types */
 import { Component, inject, OnInit } from '@angular/core';
-import { NgFor, NgIf } from '@angular/common';
+
 import { FilmService } from '../services/film.service';
 import { FilmComponent } from '../film/film.component';
 
 @Component({
   selector: 'app-list-film',
   standalone: true,
-  imports: [NgFor, NgIf, FilmComponent],
+  imports: [FilmComponent],
   templateUrl: './list-film.component.html',
   styleUrls: ['./list-film.component.css']
 })
--- src/app/list-serie/list-serie.component.ts
+++ src/app/list-serie/list-serie.component.ts
@@ -1,13 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-inferrable-types */
 import { Component, inject, OnInit } from '@angular/core';
-import { NgFor, NgIf } from '@angular/common';
+
 import { SerieService } from '../services/serie.service';
 import { SerieComponent } from '../serie/serie.component';
 
 @Component({
   selector: 'app-list-serie',
   standalone: true,
-  imports: [NgFor, NgIf, SerieComponent],
+  imports: [SerieComponent],
   templateUrl: './list-serie.component.html',
   styleUrls: ['./list-serie.component.css']
 })
